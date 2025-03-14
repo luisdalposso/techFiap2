90c0b6ff52b0c94cbe1b016bd4b6003c73b3b821
luis dalposso <luis.dalposso@gmail.com>
2025-03-13 23:06:09 -0300

    ajustes no py e no readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# techFiap2
-repositório da segunda atividade do MBA fiap
+# Script de Conversão de CSV para Parquet e Upload para S3
+
+Este script Python automatiza o processo de download de um arquivo CSV, sua conversão para o formato Parquet, e o upload do arquivo resultante para um bucket no Amazon S3. Ele faz parte de um pipeline de dados na AWS, conforme ilustrado na imagem abaixo:
+
+![Arquitetura Pipeline Dados AWS Cloud – B3](images/pipeline.png)
+
+## Descrição do Pipeline
+
+O pipeline de dados descrito na imagem segue os seguintes passos:
+
+1. **Dados IBOV (Site B3)**:
+   - Os dados são obtidos do site da B3, que fornece informações sobre o Índice Bovespa (IBOV).
+   - Um **script Python** é executado sob demanda para baixar o arquivo CSV contendo os dados.
+
+2. **AWS S3 (Bucket RAW)**:
+   - O arquivo CSV é convertido para o formato Parquet e enviado para um bucket S3 chamado `raw`.
+   - Este bucket armazena os dados brutos antes de qualquer processamento.
+
+3. **AWS Lambda (Trigger Glue)**:
+   - Um gatilho (trigger) é acionado no AWS Lambda sempre que um novo arquivo é adicionado ao bucket `raw`.
+   - O Lambda inicia um job no AWS Glue para processar os dados.
+
+4. **AWS Glue (ETL)**:
+   - O AWS Glue realiza a transformação dos dados (ETL - Extract, Transform, Load).
+   - Os dados são limpos, transformados e preparados para análise.
+
+5. **AWS S3 (Bucket REFINED)**:
+   - Após o processamento, os dados refinados são armazenados em outro bucket S3 chamado `refined`.
+
+6. **Glue Data Catalog**:
+   - O AWS Glue Data Catalog é atualizado com os metadados dos dados refinados, facilitando a consulta e análise.
+
+7. **AWS Athena**:
+   - O AWS Athena é utilizado para consultar os dados refinados diretamente do bucket `refined`, sem a necessidade de infraestrutura adicional.
+
+## Bibliotecas Utilizadas
+
+- **requests**: Para fazer requisições HTTP e baixar o arquivo CSV.
+- **pandas**: Para manipulação e limpeza dos dados.
+- **pyarrow**: Para conversão do DataFrame para o formato Parquet.
+- **boto3**: Para interagir com o Amazon S3 e fazer o upload do arquivo.
+- **unidecode**: Para normalizar caracteres Unicode.
+
+## Como Utilizar
+
+1. **Instalação das Dependências**: Certifique-se de ter todas as bibliotecas necessárias instaladas. Você pode instalá-las usando pip:
+   ```bash
+   pip install requests pandas pyarrow boto3 unidecode
--- scraper.py
+++ scraper.py
@@ -2,77 +2,42 @@ import requests
 import pandas as pd
 import pyarrow as pa
 import boto3
-from io import StringIO, BytesIO
-import base64
+from io import BytesIO
 import datetime
 import pyarrow.parquet as pq
 from unidecode import unidecode
+import base64
 
-# Obtendo a data atual e formatando o nome do arquivo
+# Configurações iniciais
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 file_name = f"carteira_diaria_{current_date}"
-
-# URL para download do arquivo CSV
 url = 'https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetDownloadPortfolioDay/eyJpbmRleCI6IklCT1YiLCJsYW5ndWFnZSI6InB0LWJyIn0='
+bucket_name = 'tech-challenge2-ibov'
 
-# Fazendo o download do arquivo CSV
+# Download do CSV
 response = requests.get(url, headers={'accept': 'application/json, text/plain, */*'})
 response.raise_for_status()
 
-# Lendo o conteúdo do CSV
-csv_content_base64 = response.content.decode('latin-1')
-csv_content = base64.b64decode(csv_content_base64).decode('latin-1')
-
-# Processando o conteúdo do CSV para remover a primeira e a última linha que não são dados em CSV
-csv_lines = csv_content.split('\n')
-csv_lines = csv_lines[2:-3]  # Remove a primeira e a última 2 linhas
-csv_lines = [unidecode(line.rstrip(';\r').replace('.','').replace('/','')) for line in csv_lines]  # Remove o ';' do final de cada linha
+# Decodificação e limpeza do conteúdo
+csv_content = response.content.decode('latin-1')
+csv_content = base64.b64decode(csv_content).decode('latin-1')
+csv_lines = csv_content.split('\n')[2:-3]
+csv_lines = [unidecode(line.rstrip(';\r').replace('.','').replace('/','')) for line in csv_lines]
 csv_content = '\n'.join(csv_lines)
 
-# Escrevendo o conteúdo do CSV para um arquivo local
-with open(f"{file_name}.csv", "w") as f:
-     f.write(csv_content)
-
-# define o type de cada coluna
-data_type = {
-    'cod_acao': 'str',
-    'desc_acao': 'str',
-    'tipo_acao': 'str',
-    'qtde_teorica': 'str',
-    'perc_part': 'str',
-    'data': 'datetime64[ns]'
-}
-
-# Lendo o arquivo CSV
-df = pd.read_csv(f"{file_name}.csv", encoding='latin-1', sep=';', header=None)
-
-# Renomeando as colunas usando seus índices
+# Leitura e transformação do DataFrame
+df = pd.read_csv(StringIO(csv_content), encoding='latin-1', sep=';', header=None)
 df.columns = ['cod_acao', 'desc_acao', 'tipo_acao', 'qtde_teorica', 'perc_part']
-
-# Adiconando coluna data
 df['data'] = current_date
-
-# Convert the 'volume' column to int
 df['qtde_teorica'] = df['qtde_teorica'].astype(int)
 
-# Convertendo o DataFrame para Parquet
+# Conversão para Parquet
 table = pa.Table.from_pandas(df)
 parquet_buffer = BytesIO()
 pq.write_table(table, parquet_buffer)
 
-# # Escrevendo o conteúdo do buffer Parquet para um arquivo local
-with open(f"{file_name}.parquet", "wb") as f:
-     f.write(parquet_buffer.getvalue())
-
-# Configurando o cliente S3
+# Upload para S3
 s3_client = boto3.client('s3', region_name='us-east-1')
+s3_client.put_object(Bucket=bucket_name, Key=f'raw/{file_name}.parquet', Body=parquet_buffer.getvalue())
 
-# Nome do bucket e do arquivo
-bucket_name = 'tech-challenge2-ibov'
-
-parquet_file_name = f'{file_name}.parquet'
-
-# Salvando o arquivo Parquet no bucket S3
-s3_client.put_object(Bucket=bucket_name, Key=f'raw/{parquet_file_name}', Body=parquet_buffer.getvalue())
-
-print(f"Arquivo salvo com sucesso no bucket {bucket_name} com o nome {parquet_file_name}")
+print(f"Arquivo salvo com sucesso no bucket {bucket_name} como {file_name}.parquet")
